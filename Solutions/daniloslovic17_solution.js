5b73c1f86e78563e10070aa401b1610eb66e2d91
daniloslovic17 <daniloslovic17@gmail.com>
2023-11-10 15:50:42 +0100

    Solutions for function until mul(...nums)

---
+++ Solutions/daniloslovic17_solution.js
@@ -0,0 +1,26 @@
+// Write a function identity that takes an argument and returns that argument
+const identity = (x) => x;
+
+// Write a binary function addb that takes two numbers and returns their sum
+const addb = (a, b) => a+b;
+
+// Write a binary function subb that takes two numbers and returns their difference
+const subb = (a, b) => a-b;
+
+// Write a binary function mulb that takes two numbers and returns their product
+const mulb = (a, b) => a*b;
+
+// Write a binary function minb that takes two numbers and returns the smaller one
+const minb = (a, b) => (a < b ? a : b);
+
+// Write a binary function maxb that takes two numbers and returns the larger one
+const maxb = (a, b) => (a > b ? a : b);
+
+// Write a function add that is generalized for any amount of arguments
+const add = (...nums) => nums.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+
+// Write a function sub that is generalized for any amount of arguments
+const sub = (...nums) => nums.reduce((accumulator, currentValue) => accumulator - currentValue, 0);
+
+// Write a function mul that is generalized for any amount of arguments
+const mul = (...nums) => nums.reduce((accumulator, currentValue) => accumulator * currentValue, 1);
